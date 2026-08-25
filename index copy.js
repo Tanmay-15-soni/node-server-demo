83c31ba1a2b9cfc6ac155756e47d325e21103c82
Tanmay-15-soni <tanmaysoni1513@gmail.com>
2026-08-25 11:49:24 +0530

    Create node.js Server

---
+++ index copy.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.get("/", (req, res) => {
+    res.send("Server is running Successfully!");
+});
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Hello from Node.js server"
+    });
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
